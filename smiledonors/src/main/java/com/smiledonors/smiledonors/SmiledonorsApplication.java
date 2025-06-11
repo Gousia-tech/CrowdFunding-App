4e985d94fc7f5092d12305a86bd5bf24736ceb5f
Gousia-tech <83952915+Gousia-tech@users.noreply.github.com>
2025-06-11 11:22:31 +0530

    Add files via upload

---
+++ smiledonors/src/main/java/com/smiledonors/smiledonors/SmiledonorsApplication.java
@@ -0,0 +1,13 @@
+package com.smiledonors.smiledonors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmiledonorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmiledonorsApplication.class, args);
+	}
+
+}
---
+++ smiledonors/src/test/java/com/smiledonors/smiledonors/SmiledonorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smiledonors.smiledonors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmiledonorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
